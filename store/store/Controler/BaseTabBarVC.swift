172b049c2da368362d2a9a3c919b42e65f21ac5c
Claudineia Souza <claudineia.r.souza@gmail.com>
2021-07-07 20:51:23 -0300

    Realizado ate as screenshots

---
+++ store/store/Controler/BaseTabBarVC.swift
@@ -0,0 +1,37 @@
+//
+//  BaseTabBarVC.swift
+//  store
+//
+//  Created by Mac Neia on 07/07/21.
+//
+
+import UIKit
+
+class BaseBarTabVC: UITabBarController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let hojeVC = self.criarTabItem(viewController: UIViewController(), titulo: "Hoje", image: "icone-hoje")
+        let appsVC = self.criarTabItem(viewController: UIViewController(), titulo: "Apps", image: "icone-apps")
+        let buscaVC = self.criarTabItem(viewController: BuscaVC(), titulo: "Busca", image: "icone-busca")
+        
+        viewControllers = [
+        hojeVC,
+        appsVC,
+        buscaVC ]
+        
+        selectedIndex = 2 
+    }
+    
+    func criarTabItem (viewController: UIViewController, titulo: String, image: String) -> UIViewController {
+        let navController = UINavigationController(rootViewController: viewController)
+        navController.navigationBar.prefersLargeTitles = true
+        
+        viewController.navigationItem.title = titulo
+        viewController.tabBarItem.title = titulo
+        viewController.tabBarItem.image = UIImage(named: image)
+        viewController.view.backgroundColor = .white
+        
+        return navController
+    }
+}
