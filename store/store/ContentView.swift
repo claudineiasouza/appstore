2c94ae9177bd76b19369e0d86a248670e5ea2a10
Claudineia Souza <claudineia.r.souza@gmail.com>
2021-07-07 11:27:35 -0300

    Commit inicial do projeto. Inclusão do projeto App Store no Github.

---
+++ store/store/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  store
+//
+//  Created by Mac Neia on 07/07/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
