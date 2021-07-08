bd4bb222e4ecbb642c568bd81820bd237b287510
Claudineia Souza <claudineia.r.souza@gmail.com>
2021-07-08 16:08:23 -0300

    Aparecendo os ícones antes de usar biblioteca de terceiros

--- store/store/View/Busca/BuscaCell.swift
+++ store/store/View/Busca/BuscaCell.swift
@@ -13,9 +13,20 @@ class BuscaCell: UITableViewCell {
         didSet {
             tituloLabel.text = app.nome
             empresaLabel.text = app.empresa
+            
+            DispatchQueue.main.async {
+                if let url = URL(string: self.app.iconeUrl) {
+                    do {
+                        let data = try Data(contentsOf: url)
+                        self.iconeImageView.image = UIImage(data: data)
+                    } catch let err {
+                            print(err)
+                        }
+                    }
+                }
+            }
         }
-    }
-    
+
     let iconeImageView: UIImageView = .iconeImageView()
     let tituloLabel: UILabel = .textLabel(text: "App nome", fontSize: 18, numberOfLines: 2)
     let empresaLabel: UILabel = .textLabel(text: "Empresa nome", fontSize: 14)
