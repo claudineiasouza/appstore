7cac80d3957256dac26de843a654118bda43f369
Claudineia Souza <claudineia.r.souza@gmail.com>
2021-07-13 10:45:20 -0300

    Tamanhos das Células App detalhe header layout personalizado

--- store/store/Controler/AppDetalhe /AppDetalheVC.swift
+++ store/store/Controler/AppDetalhe /AppDetalheVC.swift
@@ -7,9 +7,9 @@
 
 import UIKit
 
-class AppDetalheVC: UICollectionViewController {
+class AppDetalheVC: UICollectionViewController, UICollectionViewDelegateFlowLayout {
     
-    let cellId = "cellId"
+    let headerId = "headerId"
     
     init() {
         super.init(collectionViewLayout: UICollectionViewFlowLayout())
@@ -21,17 +21,26 @@ class AppDetalheVC: UICollectionViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        navigationItem.largeTitleDisplayMode = .never // para ficar pequeno
+        
         collectionView.backgroundColor = .white
-        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: cellId)
+        collectionView.register(AppDetalheHeaderCell.self, forCellWithReuseIdentifier: headerId)
     }
     
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 4
+        return 1
     }
     
     override func collectionView( _ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath)
-        cell.backgroundColor = .red
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: headerId, for: indexPath) as! AppDetalheHeaderCell
         return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        
+        let width: CGFloat = view.bounds.width
+        let height: CGFloat = 170
+        
+        return .init(width: width, height: height)
     }
 }
---
+++ store/store/View/AppDetalhe/AppDetalheHeaderCell.swift
@@ -0,0 +1,39 @@
+//
+//  AppDetalheHeaderCell.swift
+//  store
+//
+//  Created by Mac Neia on 13/07/21.
+//
+
+import UIKit
+
+class AppDetalheHeaderCell: UICollectionViewCell {
+    
+    let iconeImageView: UIImageView = .iconeImageView(width: 128, heigth: 128)
+    let nomeLabel: UILabel = .textLabel(text: "App nome", fontSize: 20, numberOfLines: 2)
+    let empresaLabel: UILabel = .textLabel(text: "App empresa", fontSize: 14)
+    let obterButtuon: UIButton = .obterButton()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        iconeImageView.image = UIImage(named: "icone")
+        
+        let buttonstackView = UIStackView(arrangedSubviews: [obterButtuon, UIView()])
+        
+        let tituloEmpresaStackView = UIStackView(arrangedSubviews: [nomeLabel, empresaLabel, buttonstackView])
+        tituloEmpresaStackView.spacing = 12
+        tituloEmpresaStackView.axis = .vertical
+        
+        let stackView = UIStackView(arrangedSubviews: [iconeImageView, tituloEmpresaStackView])
+        stackView.alignment = .center
+        stackView.spacing = 12
+        
+        addSubview(stackView)
+        stackView.preencherSuperview(padding: .init(top: 0, left: 20, bottom: 0, right: 20))
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError()
+    }
+}
