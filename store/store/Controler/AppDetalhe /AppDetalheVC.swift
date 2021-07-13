b2e47d9f96fc2f93313282d08c7ea329c328a5b1
Claudineia Souza <claudineia.r.souza@gmail.com>
2021-07-13 13:47:58 -0300

    Descrição  e altura dinamica.

--- store/store/Controler/AppDetalhe /AppDetalheVC.swift
+++ store/store/Controler/AppDetalhe /AppDetalheVC.swift
@@ -10,6 +10,7 @@ import UIKit
 class AppDetalheVC: UICollectionViewController, UICollectionViewDelegateFlowLayout {
     
     let headerId = "headerId"
+    let descricaoId = "descricaoId"
     
     init() {
         super.init(collectionViewLayout: UICollectionViewFlowLayout())
@@ -25,21 +26,41 @@ class AppDetalheVC: UICollectionViewController, UICollectionViewDelegateFlowLayo
         
         collectionView.backgroundColor = .white
         collectionView.register(AppDetalheHeaderCell.self, forCellWithReuseIdentifier: headerId)
+        collectionView.register(AppDetalheDescricaoCell.self, forCellWithReuseIdentifier: descricaoId)
     }
     
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 1
+        return 2
     }
     
     override func collectionView( _ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: headerId, for: indexPath) as! AppDetalheHeaderCell
-        return cell
-    }
+        
+        if indexPath.item == 0 {
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: headerId, for: indexPath) as! AppDetalheHeaderCell
+            return cell
+        }
+       
+        if indexPath.item == 1 {
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: descricaoId, for: indexPath) as! AppDetalheDescricaoCell
+            return cell
+        }
+        
+        return UICollectionViewCell()
+        
+       }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
         
         let width: CGFloat = view.bounds.width
-        let height: CGFloat = 170
+        var height: CGFloat = 170
+        
+        if indexPath.item == 1 {
+            let descricaocell = AppDetalheDescricaoCell(frame: CGRect(x: 0, y: 0, width: width, height: 1000)) //celula ficticia
+            descricaocell.layoutIfNeeded()
+            
+            let estimativaTamanhoCell = descricaocell.systemLayoutSizeFitting(CGSize(width: width, height: 1000))
+            height = estimativaTamanhoCell.height // para ficar do tamanho do texto
+        }
         
         return .init(width: width, height: height)
     }
---
+++ store/store/View/AppDetalhe/AppDetalheDescricaoCell.swift
@@ -0,0 +1,29 @@
+//
+//  AppDetalheDescricaoCell.swift
+//  store
+//
+//  Created by Mac Neia on 13/07/21.
+//
+
+import UIKit
+
+class AppDetalheDescricaoCell: UICollectionViewCell {
+    
+    let tituloLabel: UILabel = .textboldLabel(text: "Novidades", fontSize: 24)
+    let descricaoLabel: UILabel = .textLabel(text: "Descrição", fontSize: 12, numberOfLines: 0)
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        let stackView = UIStackView(arrangedSubviews: [tituloLabel, descricaoLabel])
+        stackView.spacing = 12
+        stackView.axis = .vertical
+        
+      addSubview(stackView)
+        stackView.preencherSuperview(padding: .init(top: 0, left: 20, bottom: 0, right: 20))
+        
+    }
+    required init?(coder: NSCoder) {
+        fatalError()
+    }
+}
