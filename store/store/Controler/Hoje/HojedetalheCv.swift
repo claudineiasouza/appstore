c554209ed9dbe6c4476e21e8352d3405bdfcdf3e
Claudineia Souza <claudineia.r.souza@gmail.com>
2021-07-15 14:32:34 -0300

    Aplicando animação de abertura no view.

--- store/store/Controler/Hoje/HojedetalheCv.swift
+++ store/store/Controler/Hoje/HojedetalheCv.swift
@@ -12,10 +12,15 @@ class HojedetalheCv: UIViewController {
     var centerView: UIView?
     var frame: CGRect?
     
+    var topConstraint: NSLayoutConstraint?
+    var leadingConstraint: NSLayoutConstraint?
+    var widthConstraint: NSLayoutConstraint?
+    var heightConstraint: NSLayoutConstraint?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.backgroundColor = .yellow
+        view.backgroundColor = .clear // clear tranparente
         
         centerView = UIView()
     }
@@ -25,11 +30,38 @@ class HojedetalheCv: UIViewController {
         guard let centerView = self.centerView else {return}
         guard let frame = self.frame else {return}
         
+        centerView.translatesAutoresizingMaskIntoConstraints = false
         centerView.backgroundColor = .blue
-        centerView.frame = frame
+        centerView.layer.cornerRadius = 16
+        centerView.clipsToBounds = true
         
         view.addSubview(centerView)
         
+        self.topConstraint = centerView.topAnchor.constraint(equalTo: view.topAnchor, constant: frame.origin.y)
+        self.leadingConstraint = centerView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: frame.origin.x)
+        self.widthConstraint = centerView.widthAnchor.constraint(equalToConstant: frame.width)
+        self.heightConstraint = centerView.heightAnchor.constraint(equalToConstant: frame.height)
+        
+        self.topConstraint?.isActive = true
+        self.leadingConstraint?.isActive = true
+        self.widthConstraint?.isActive = true
+        self.heightConstraint?.isActive = true
+        
+        view.layoutIfNeeded()
+        
+        UIView.animate(withDuration: 0.3, delay: 0.0, options: .overrideInheritedCurve, animations: {
+            
+            self.topConstraint?.constant = 0
+            self.leadingConstraint?.constant = 0
+            self.widthConstraint?.constant = self.view.frame.width
+            self.heightConstraint?.constant = self.view.frame.height
+            
+            self.centerView?.layer.cornerRadius = 0
+            
+            self.view.layoutIfNeeded()
+            
+        }, completion: nil)
+            
+        }
     }
     
-}
